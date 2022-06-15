b6c781c03e9733d0e6448d0cb17e9a3831d1382e
Shay444 <86199753+shay444ofc@users.noreply.github.com>
2022-06-15 00:45:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let GetVersion = require("./src/version.js");
 let GetServerInfo = require("./src/server.js");
 let redeemCode = require("./src/reward.js");
+let getAccountCreationDate = require("./src/data.js");
 
 
 module.exports = {
     GetVersion,
     GetServerInfo,
-    redeemCode
+    redeemCode,
+    getAccountCreationDate
 }
